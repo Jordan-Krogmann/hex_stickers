b3a32661d0a4993d910649e23b9d72510a8b55fe
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-01-10 10:59:09 -0500

    cleaned code

--- R/one_team_one_goat.R
+++ R/one_team_one_goat.R
@@ -4,19 +4,25 @@
 # install.packages("hexSticker")
 library(hexSticker)
 
-# read image
-imgurl <- system.file(
-  here::here("imgs","goat.png")
-  , package = "hexSticker"
-  )
+# path to img
+sticker_path <- file.path(here::here("imgs","goat.png"))
+
 
 # create hex sticker
 sticker(
-  imgurl
-  , package = "hexSticker"
-  , p_size = 20
+  sticker_path
+  , package = "One Team One Goat"
+  , p_color = "dodgerblue4"
+  , h_color = "dodgerblue4"
+  , h_fill = "white"
+  , p_size = 80
   , s_x = 1
   , s_y = .75
   , s_width = .6
-  , filename = "inst/figures/imgfile.png"
+  , filename = here::here("imgs","goat_hex.png")
+  , dpi = 2000
   )
+
+
+
+
