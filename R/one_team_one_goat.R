3aae9b80733e391f4915046af98fdb90e1d40518
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-01-30 16:08:20 -0500

    small edit

--- R/one_team_one_goat.R
+++ R/one_team_one_goat.R
@@ -5,30 +5,30 @@
 library(hexSticker)
 
 # path to img
-sticker_path <- file.path(here::here("imgs","mranda.png"))
+sticker_path <- file.path(here::here("imgs","sextant_white_2.png"))
 
 
 # create hex sticker
 sticker(
 sticker_path
-  , package = "ReMaT"
-  , p_color = "seashell"
+  , package = "ReMap"
+  , p_color = "white"
   , p_x = 1
-  , p_y = .35
-  , h_color = "seashell"
+  , p_y = 1.55
+  , h_color = "white"
   , h_fill = "dodgerblue4"
-  , p_size = 80
+  , p_size = 110
   , s_x = 1
-  , s_y = 1.2
-  , s_width = .8
-  , s_height = .8
-  , filename = here::here("imgs","mranda_hex.png")
+  , s_y = .85
+  , s_width = .6
+  , s_height = .6
+  , filename = here::here("imgs","sextant_hex.png")
   , dpi = 2000
   , url = "challenge the status quo"
-  , u_color = "seashell"
-  , u_size = 30
-  , u_x = .35
-  , u_y = 1.57
+  , u_color = "white"
+  , u_size = 35
+  , u_x = 1.1
+  , u_y = .15
   )
 
 
