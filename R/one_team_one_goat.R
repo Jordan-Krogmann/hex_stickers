486843b7dc64df8e0ab93d0521eb146a04924b84
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-03-11 10:20:23 -0400

    edits

--- R/one_team_one_goat.R
+++ R/one_team_one_goat.R
@@ -5,27 +5,29 @@
 library(hexSticker)
 
 # path to img
-sticker_path <- file.path(here::here("imgs","sextant_white.png"))
+sticker_path <- file.path(here::here("imgs","voc.png"))
 
 
 # create hex sticker
 sticker(
 sticker_path
-  , package = "ReMap"
-  , p_color = "white"
+  
+
+  , h_color = "#727577"
+  , h_fill = "#EFEFEF"
+  , package = "Voice Of Customer"
+  , p_color = "#0A3770"
   , p_x = 1
-  , p_y = 1.55
-  , h_color = "white"
-  , h_fill = "dodgerblue4"
-  , p_size = 110
+  , p_y = 1.60
+  , p_size = 55
   , s_x = 1
-  , s_y = .85
-  , s_width = .6
+  , s_y = .95
+  , s_width = .85
   , s_height = .6
-  , filename = here::here("imgs","sextant_hex.png")
+  , filename = here::here("imgs","voc_logo_hex.png")
   , dpi = 2000
   , url = "challenge the status quo"
-  , u_color = "white"
+  , u_color = "#0A3770"
   , u_size = 35
   , u_x = 1.1
   , u_y = .15
