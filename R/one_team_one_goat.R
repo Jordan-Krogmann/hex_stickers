bbcf737dd1fbdb6c3147cb697437aafa04963d37
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-03-20 18:12:44 -0400

    fixed script

--- R/one_team_one_goat.R
+++ R/one_team_one_goat.R
@@ -3,33 +3,41 @@
 
 # install.packages("hexSticker")
 library(hexSticker)
+library(showtext)
+
+font_add_google("Gochi Hand", "gochi")
+## Automatically use showtext to render text for future devices
+showtext_auto()
+
 
 # path to img
-sticker_path <- file.path(here::here("imgs","voc.png"))
+sticker_path <- file.path(here::here("imgs","martec.png"))
 
 
 # create hex sticker
 sticker(
     sticker_path
-  , h_size = 1.5
+  , h_size = 1.4
   , h_color = "#727577"
-  , h_fill = "#EFEFEF"
-  , package = "Voice Of Customer"
-  , p_color = "#0A3770"
+  , h_fill = "#071b39"
+  # , package = "ReMap: Plus One"
+  # , p_family = "gochi"
+  # , p_color = "#fdb913"
   , p_x = 1
-  , p_y = 1.40
-  , p_size = 85
-  , s_x = 1
-  , s_y = .84
+  , p_y = 1.45
+  , p_size = 100
+  , s_x = .97
+  , s_y = .82
   , s_width = .6
   , s_height = .6
-  , filename = here::here("imgs","voc_logo_hex.png")
+  , filename = here::here("imgs","matec_hex.png")
   , dpi = 2000
-  , url = "challenge the status quo"
-  , u_color = "#0A3770"
-  , u_size = 45
-  , u_x = .95
-  , u_y = .09
+  # , url = "measure once, test never"
+  # , u_color = "#fdb913"
+  # , u_family = "gochi"
+  # , u_size = 48
+  # , u_x = .99
+  # , u_y = .085
   )
 
 
