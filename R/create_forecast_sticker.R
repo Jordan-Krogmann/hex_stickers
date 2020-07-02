bf92595554be12b66664f92d5ddcb2b95b44d496
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-07-02 11:13:49 -0400

    added create forecast sticker script

--- R/create_forecast_sticker.R
+++ R/create_forecast_sticker.R
@@ -6,6 +6,10 @@ library(showtext)
 library(tidyverse)
 library(snhuthemes)
 
+
+font_add_google("Oswald")
+showtext_auto()
+
 # adding forecast plot
 x <- seq(0,1000, by = .5)
 
@@ -17,29 +21,35 @@ df <- df %>%
   rename(x = value )
 
 fplt <- df %>% 
-  filter(x <= 10) %>%
+  filter(x <= 5) %>%
   ggplot(aes(x = x)) + 
-  geom_line(aes(y = x_square),  color = "dodgerblue3") + 
-  geom_point(aes(y = y), size = .5, color = "firebrick4") +
-  geom_point(aes(y = x_square),size = .55,  color = "dodgerblue4") +
+  
+  geom_line(aes(y = y), size = 1.25, color = "firebrick4", linetype = 1) +
+  geom_point(aes(y = y), size = 2, color = "firebrick4") +
+  geom_line(aes(y = x_square), size = 1.5,  color = "#3E63AD") + 
+  geom_point(aes(y = x_square), size = 2.25,  color = "#3E63AD") +
   theme_void() + 
   theme_transparent()
 
 sticker(
   fplt,
-  package = "SNHU Forecasting", 
-  p_color = "#23395b",
-  p_size = 14.5, 
-  p_y = 1.45,
+  package = " ", 
+  p_color = "#EFEFEF",
+  p_size = 27.5, 
+  p_family = "Oswald",
+  p_y = 1,
   s_x = 1, 
-  s_y = .81, 
-  s_width = 1, 
+  s_y = .95, 
+  s_width = 1.5, 
   s_height = 1,
-  h_fill = "#EFEFEF",
-  h_color = "#000000",
-  filename = here::here("imgs","forecast_hex.png"),
+  h_fill = "#23395b",
+  h_color = "#EFEFEF",
+  filename = here::here("imgs","forecast_hex_alt.png"),
   url = "The best qualification of a prophet is to have a good memory.",
-  u_size = 2.7
+  u_size = 5.5,
+  u_color = "#FFFFFF",
+  u_family = "Oswald",
+  dpi = 500
 )
 
 
