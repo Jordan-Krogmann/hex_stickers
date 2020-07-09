5796ca3880e5da8080da0cf838dd4fb01fe1182d
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-07-09 15:43:17 -0400

    fixed sticker path

--- R/one_team_one_goat.R
+++ R/one_team_one_goat.R
@@ -5,39 +5,39 @@
 library(hexSticker)
 library(showtext)
 
-font_add_google("Gochi Hand", "gochi")
-## Automatically use showtext to render text for future devices
+# font_add_google("Gochi Hand", "gochi")
+# ## Automatically use showtext to render text for future devices
+# showtext_auto()
+font_add_google("Oswald")
 showtext_auto()
 
+
 # path to img
-sticker_path <- file.path(here::here("imgs","sandbag.jpg"))
+sticker_path <- file.path(here::here("imgs","analytics_alt.jpg"))
+
+
 
 # create hex sticker
 sticker(
-    fplt
-  , h_size = 1.4
-  , h_color = "#000000"
-  , h_fill = "#FFFFFF"
-  , package = "Making fOrEcASts"
-  , p_family = "gochi"
-  , p_color = "#000000"
-  , p_x = 1
-  , p_y = .4
-  , p_size = 80
-  , s_x = 1
-  , s_y = 1
-  , s_width = .5
-  , s_height = .5
-  , filename = here::here("imgs","forecast_hex_alt.png")
-  , dpi = 2000
-  , url = "Being Conservative for Convservative's Sake"
-  , u_color = "#000000"
-  , u_family = "gochi"
-  , u_size = 30
-  , u_x = .17
-  , u_y = 1.38
-  )
-
+  sticker_path ,
+  package = " ", 
+  p_color = "#EFEFEF",
+  p_size = 27.5, 
+  p_family = "Oswald",
+  p_y = 1,
+  s_x = 1, 
+  s_y = .95, 
+  s_width = 1.5, 
+  s_height = 1,
+  h_fill = "#23395b",
+  h_color = "#071b39",
+  filename = here::here("imgs","forecast_hex_alt.png"),
+  url = "The best qualification of a prophet is to have a good memory.",
+  u_size = 5.5,
+  u_color = "#FFFFFF",
+  u_family = "Oswald",
+  dpi = 500
+)
 
 
 
