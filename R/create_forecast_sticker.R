c87ff6d49726396ecaccfccedc1054a889bb24f8
Jordan-Krogmann <jordanckrogmann@gmail.com>
2020-07-09 16:52:05 -0400

    unmessed up stuff

--- R/create_forecast_sticker.R
+++ R/create_forecast_sticker.R
@@ -11,28 +11,26 @@ font_add_google("Oswald")
 showtext_auto()
 
 # adding forecast plot
-x <- seq(0,1000, by = .5)
+df <- tribble(
+   ~x,   ~y,     ~z,
+    1,   .5,    .75,
+    2,    1,   1.25,
+    3,  .75,      1,
+    4,  1.25,   1.75,
+    5,  1,   1.5
+)
 
-df <- as_tibble(x = x)
 
-df <- df %>% 
-  mutate(x_square = x^1.25) %>% 
-  mutate(y = x) %>% 
-  rename(x = value )
-
-fplt <- df %>% 
+p <- df %>% 
   filter(x <= 5) %>%
   ggplot(aes(x = x)) + 
-  
-  geom_line(aes(y = y), size = 1.25, color = "firebrick4", linetype = 1) +
-  geom_point(aes(y = y), size = 2, color = "firebrick4") +
-  geom_line(aes(y = x_square), size = 1.5,  color = "#3E63AD") + 
-  geom_point(aes(y = x_square), size = 2.25,  color = "#3E63AD") +
+  geom_col(aes(y = y), size = 1.25, color = "firebrick4", linetype = 1) +
+  geom_line(aes(y = z), size = 1, color = "firebrick4") +
   theme_void() + 
   theme_transparent()
 
 sticker(
-  fplt,
+  p,
   package = " ", 
   p_color = "#EFEFEF",
   p_size = 27.5, 
@@ -40,13 +38,13 @@ sticker(
   p_y = 1,
   s_x = 1, 
   s_y = .95, 
-  s_width = 1.5, 
+  s_width = 1, 
   s_height = 1,
-  h_fill = "#23395b",
-  h_color = "#EFEFEF",
-  filename = here::here("imgs","forecast_hex_alt.png"),
-  url = "The best qualification of a prophet is to have a good memory.",
-  u_size = 5.5,
+  h_fill = "#0a3370",
+  h_color = "#071b39",
+  filename = here::here("imgs","analytics_hex_alt.png"),
+  url = "In God we trust, all others must bring data.",
+  u_size = 8,
   u_color = "#FFFFFF",
   u_family = "Oswald",
   dpi = 500
